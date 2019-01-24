582452cab98cfe70e8bf2ef1b61839781118005d
Search <yaodong199@icloud.com>
2019-01-24 16:38:14 +0800

    增加method2 丑数

--- src/main/java/algorithm/math/NthUglyNumber_264.java
+++ src/main/java/algorithm/math/NthUglyNumber_264.java
@@ -32,4 +32,24 @@ public class NthUglyNumber_264 {
         }
         return dp[n - 1];
     }
+	
+	/**
+	* method2
+	*
+    * @param n
+    * @return
+    */
+    public int nthUglyNumber2(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int t2 = 0, t3 = 0, t5 = 0, index = 1;
+        while (index < n) {
+            res[index] = Math.min(res[t2] * 2, Math.min(res[t3] * 3, res[t5] * 5));
+            t2 += res[index] == res[t2] * 2 ? 1 : 0;
+            t3 += res[index] == res[t3] * 3 ? 1 : 0;
+            t5 += res[index] == res[t5] * 5 ? 1 : 0;
+            ++index;
+        }
+        return res[n - 1];
+    }
 }
