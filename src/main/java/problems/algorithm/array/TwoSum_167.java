c6de817020a1ca7b4fe0338afbe922fe2aabd315
Search <yaodong199@icloud.com>
2019-06-17 09:46:04 +0800

    二分搜索及双指针版本

--- src/main/java/problems/algorithm/array/TwoSum_167.java
+++ src/main/java/problems/algorithm/array/TwoSum_167.java
@@ -27,4 +27,64 @@ public class TwoSum_167 {
 		}
 		throw new IllegalArgumentException("No two sum solution");
 	}
+	
+	// ¶þ·ÖËÑË÷
+	public int[] twoSumV2(int[] numbers, int target) {
+
+        if(numbers.length < 2 /*|| !isSorted(numbers)*/)
+            throw new IllegalArgumentException("Illegal argument numbers");
+
+        for(int i = 0 ; i < numbers.length - 1 ; i ++){
+            int j = binarySearch(numbers, i+1, numbers.length-1, target - numbers[i]);
+            if(j != -1){
+                int[] res = {i+1, j+1};
+                return res;
+            }
+        }
+
+        throw new IllegalStateException("The input has no solution");
+    }
+
+    private int binarySearch(int[] nums, int l, int r, int target){
+
+        if(l < 0 || l > nums.length)
+            throw new IllegalArgumentException("l is out of bound");
+
+        if(r < 0 || r > nums.length)
+            throw new IllegalArgumentException("r is out of bound");
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(target > nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return -1;
+    }
+    
+    //Ë«Ö¸Õë
+    public int[] twoSumV3(int[] numbers, int target) {
+
+        if(numbers.length < 2 /*|| !isSorted(numbers)*/)
+            throw new IllegalArgumentException("Illegal argument numbers");
+
+        int l = 0, r = numbers.length - 1;
+        while(l < r){
+
+            if(numbers[l] + numbers[r] == target){
+                int[] res = {l+1, r+1};
+                return res;
+            }
+            else if(numbers[l] + numbers[r] < target)
+                l ++;
+            else // numbers[l] + numbers[r] > target
+                r --;
+        }
+
+        throw new IllegalStateException("The input has no solution");
+    }
 }
