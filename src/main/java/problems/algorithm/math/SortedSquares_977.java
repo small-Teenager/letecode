d4302214436fd9e5675fdb9cc825b46dc4793ba0
Search <yaodong199@icloud.com>
2019-06-29 02:50:32 +0800

    有序数组的平方

--- src/main/java/problems/algorithm/math/SortedSquares_977.java
+++ src/main/java/problems/algorithm/math/SortedSquares_977.java
@@ -25,5 +25,24 @@ public class SortedSquares_977 {
 		Arrays.sort(A);
 		return A;
 	}
+	
+	//V2数组有序，则数组的第一个元素的平方与最后一个元素的平方中的最大值一定为整个数组的最大值，故给定2个指针，一个为从0开始，一个为最大值
+	public int[] sortedSquaresV2(int[] A) {
+        int maxIndex = A.length;
+        int beforeIndex = 0;
+        int afterIndex = maxIndex-1;
+        int head = maxIndex-1;
+        int[] resultArr = new int[maxIndex];
+        while(beforeIndex != afterIndex){
+            if(A[beforeIndex]*A[beforeIndex] >= A[afterIndex]*A[afterIndex]){
+                resultArr[head--] = A[beforeIndex]*A[beforeIndex++];
+            }else{
+                resultArr[head--] = A[afterIndex]*A[afterIndex--];
+            }
+        }
+        resultArr[0] = A[beforeIndex]*A[beforeIndex];
+        return resultArr;
+    }
+
 
 }
