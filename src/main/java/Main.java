919d75c4d68ce2fe269d76d28ccafdc7a9405baa
qtj_zhangyaodong <yaodong199@icloud.com>
2019-10-15 14:48:04 +0800

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Search
+ * @version 1.0
+ * @date 2019/9/12 14:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(200 * 300 * 400 * 500);
+    }
+}
