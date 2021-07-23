1e8cedde7b51595c0a9cf758f842e12ac1fb5a27
zhangyaodong <yaodong199@icloud.com>
2021-07-23 20:42:55 +0800

    add  1 and 2

---
+++ problem100/0002.py
@@ -0,0 +1,39 @@
+'''
+给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0开头
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        re = ListNode(0)
+        r = re
+        carry = 0
+        while (l1 or l2):
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            s = carry + x + y
+            carry = s // 10
+            r.next = ListNode(s % 10)
+            r = r.next
+            if (l1 != None): l1 = l1.next
+            if (l2 != None): l2 = l2.next
+        if (carry > 0):
+            r.next = ListNode(1)
+        return re.next
